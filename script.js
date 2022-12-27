8faf547da0cb4fa197d5137ff98aacababa12d5f
Gabriela Valle <vallefarinha@gmail.com>
2022-12-27 12:17:33 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+function meuEscopo() {
+    const form = document.querySelector('.form');
+    const resultado = document.querySelector('.resultado');
+    const pessoas = [];
+
+    function recebeEventoForm(e) {
+        e.preventDefault();
+        const nome = form.querySelector('.nome');
+        const sobrenome = form.querySelector('.sobrenome');
+        const peso = form.querySelector('.peso');
+        const altura = form.querySelector('.altura');
+
+        pessoas.push = ({
+            nome: nome.value,
+            sobrenome: sobrenome.value,
+            peso: peso.value,
+            altura: altura.value
+        });
+
+        resultado.innerHTML += `<p>  ${nome.value} ${sobrenome.value} - ` + ` peso: ${peso.value}, altura: ${altura.value}</p>`;
+
+    }
+    form.addEventListener('submit', recebeEventoForm);
+}
+
+meuEscopo();
